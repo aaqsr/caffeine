02ca04ee6cd408b5eae4b4c78b2f7556c6f03b52
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-10 23:53:46 +0500

    Added NavBar component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { Navbar } from "react-bootstrap";
+
+
+export const NavBar: FC = () => (
+    <Navbar bg="light">
+        <Navbar.Brand href="#home">CAFFEINE</Navbar.Brand>
+    </Navbar>
+);
