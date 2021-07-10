8fe0972507f916de96724711584b4961718cbd06
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-10 22:20:23 +0500

    Got rid of default code

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const IndexPage: React.FC = () => {
+    return (
+        <h1>hi</h1>
+    );
+}
+
+export default IndexPage;
