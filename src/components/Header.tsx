66cb48ebd4b60233b45b173aacdfda2d7333aa29
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-11 16:41:52 +0500

    Added max image size and centered it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import "../styles/header.scss";
 import Caffeine_structure from "../images/Caffeine_structure.svg";
 
 export const Header: FC = () => (
-    <div className="Header">
+    <div className="header">
         <Jumbotron fluid>
             <h1 className="center">
                 <Fade bottom cascade>
@@ -15,7 +15,9 @@ export const Header: FC = () => (
                 </Fade>
             </h1>
             <Fade bottom delay={1000}>
-                <img src={Caffeine_structure} alt="Caffeine Skeletal Formula"/>
+                <div className="center">
+                    <img src={Caffeine_structure} className="img" alt="Caffeine Skeletal Formula"/>
+                </div>
             </Fade>
         </Jumbotron>
     </div>
