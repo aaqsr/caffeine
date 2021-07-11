4c0f6ed9f9779172cae4ecca8032fdcc2b43a0be
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-11 12:10:24 +0500

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { FC } from 'react';
+import { Fade } from 'react-bootstrap';
+
+export const Header: FC = () => (
+    <h1>
+        <Fade bottom cascade> 
+            CAFFEINE
+        </Fade>
+    </h1>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { FC } from "react";
 import { NavBar } from "../components/NavBar";
+import { Header } from "../components/Header";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/font.scss";
@@ -8,7 +9,7 @@ import "../styles/font.scss";
 const IndexPage: FC = () => (
     <div>
         <NavBar />
-        <h1>hi</h1>
+        <Header />
     </div>
 )
 
