9ee0f2e7ed968a41a2cb7197819a08ace9e7be3c
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-11 16:42:02 +0500

    Made dumb workaround

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,33 +3,37 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { FC } from "react";
 import { Navbar } from "react-bootstrap";
-// import { Slide } from "react-reveal";
+import { Slide } from "react-reveal";
 
 import "../styles/navbar.scss";
 
 export const NavBar: FC = () => {
     const [visibility, setVisibility] = useState(false);
+    const [visibility2, setVisibility2] = useState(false);
 
     useEffect(() => {
         if (typeof window !== undefined) {
             const changeVisibility = () => {
-                setVisibility(!!(window.scrollY >= 80));
+                setVisibility(!!(window.scrollY >= 100));
+                setVisibility2(!!(window.scrollY >= 130));
             };
 
             window.addEventListener("scroll", changeVisibility);
         }
     }, []);
 
-    console.log(visibility);
+    // console.log(visibility);
 
     if (visibility) {
         return (
             <div className="NavBar">
-                    <Navbar fixed="top" bg="light" className="navbar">
+                <Navbar fixed="top" bg="light" className="navbar">
+                    <Slide left when={visibility2}>
                         <Navbar.Brand href="#home">
                             <h2>CAFFEINE</h2>
                         </Navbar.Brand>
-                    </Navbar>
+                    </Slide>
+                </Navbar>
             </div>
         );
     }
