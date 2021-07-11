b03df08fa72ada6304a0c42af8cd2329fcd63fbb
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-11 13:07:19 +0500

    Made NavBar appear when scrolled down certain length

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,29 @@
 import * as React from "react";
+import { useState } from "react";
 import { FC } from "react";
 import { Navbar } from "react-bootstrap";
 
 import "../styles/navbar.scss";
 
-export const NavBar: FC = () => (
-    <div className="NavBar">
-        <Navbar bg="light" className="navbar">
-            <Navbar.Brand href="#home">
-                <h2>CAFFEINE</h2>
-            </Navbar.Brand>
-        </Navbar>
-    </div>
-);
+export const NavBar: FC = () => {
+    const [visibility, setVisibility] = useState(false);
+
+    const changeVisibility = () => {
+        setVisibility(!!(window.scrollY >= 80))
+    }
+
+    window.addEventListener('scroll', changeVisibility)
+
+    if (visibility) {
+        return (
+            <div className="NavBar">
+                <Navbar bg="light" className="navbar">
+                    <Navbar.Brand href="#home">
+                        <h2>CAFFEINE</h2>
+                    </Navbar.Brand>
+                </Navbar>
+            </div>
+        )
+    }
+    return null;
+}
