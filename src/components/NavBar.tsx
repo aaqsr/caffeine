b1bf24b1583c3659615b8f14cd29571d7ea605b5
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-11 14:07:33 +0500

    Slide animation failed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { FC } from "react";
 import { Navbar } from "react-bootstrap";
-import { Slide } from "react-reveal";
+// import { Slide } from "react-reveal";
 
 import "../styles/navbar.scss";
 
@@ -14,22 +14,24 @@ export const NavBar: FC = () => {
         if (typeof window !== undefined) {
             const changeVisibility = () => {
                 setVisibility(!!(window.scrollY >= 80));
-            }
+            };
 
             window.addEventListener("scroll", changeVisibility);
         }
     }, []);
 
+    console.log(visibility);
+
     if (visibility) {
         return (
             <div className="NavBar">
-                <Navbar fixed="top" bg="light" className="navbar">
-                    <Navbar.Brand href="#home">
-                        <h2>CAFFEINE</h2>
-                    </Navbar.Brand>
-                </Navbar>
+                    <Navbar fixed="top" bg="light" className="navbar">
+                        <Navbar.Brand href="#home">
+                            <h2>CAFFEINE</h2>
+                        </Navbar.Brand>
+                    </Navbar>
             </div>
         );
     }
     return null;
-}
+};
