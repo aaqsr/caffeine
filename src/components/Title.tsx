ecdeb6b1f9cda2a5935c29a77da2d3de88f09d79
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-13 20:05:50 +0500

    Changes

---
+++ src/components/Title.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { FC } from "react";
+import { Jumbotron } from "react-bootstrap";
+import { Fade } from "react-reveal";
+
+import "../styles/header.scss";
+import Caffeine_structure from "../images/Caffeine_structure.svg";
+
+export const Title: FC = () => (
+    <div className="header">
+        <Jumbotron fluid>
+            <h1 className="center">
+                <Fade bottom cascade>
+                    CAFFEINE
+                </Fade>
+            </h1>
+            <Fade bottom delay={1000}>
+                <div className="center">
+                    <img src={Caffeine_structure} className="img" alt="Caffeine Skeletal Formula"/>
+                </div>
+            </Fade>
+        </Jumbotron>
+    </div>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
 import { FC } from "react";
-import { NavBar } from "../components/NavBar";
+
 import { Header } from "../components/Header";
+import { NavBar } from "../components/NavBar";
+import { Title } from "../components/Title";
 import { DummyText } from "../components/DummyText"
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/font.scss";
+import "../styles/general.scss";
 
 const IndexPage: FC = () => (
-    <div>
-        <NavBar />
+    <>
         <Header />
+        <NavBar />
+        <Title />
         <DummyText />
-    </div>
+    </>
 )
 
 export default IndexPage;
