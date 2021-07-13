7786a15d38535bbced375bfa50ae4f256248a3e8
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-13 20:21:57 +0500

    minor changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { FC } from "react";
 
 import { Header } from "../components/Header";
 import { NavBar } from "../components/NavBar";
@@ -9,7 +8,7 @@ import { DummyText } from "../components/DummyText"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/general.scss";
 
-const IndexPage: FC = () => (
+const IndexPage: React.FC = () => (
     <>
         <Header />
         <NavBar />
