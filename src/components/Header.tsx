8808687ef451a37b1bd454b0259330b1cc8514ce
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-13 20:05:18 +0500

    formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
+import * as React from "react";
 import { Helmet } from "react-helmet";
 
-import img from '../images/CaffeineInCoffeeBeans.jpeg';
+import img from "../images/CaffeineInCoffeeBeans.jpeg";
 
 const metaData = {
     title: "Caffeine",
