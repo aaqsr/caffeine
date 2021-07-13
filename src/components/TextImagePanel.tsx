d5a47a92c8172fe2a147ce1202cf6ff3b482ba06
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-13 21:41:19 +0500

    Added prop types

--- src/components/TextImagePanel.tsx
+++ src/components/TextImagePanel.tsx
@@ -4,14 +4,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { Text } from "./BodyText";
 import { Image } from "./BodyImage";
 
+interface PanelProps {
+    heading: string,
+    subheading?: string,
+    body: string,
+    children: any
+    image: any
+}
 
-export const Panel: React.FC = props => (
+export const Panel: React.FC<PanelProps> = props => (
     <Container>
         <Row>
-            <Text>
+            <Text heading={props.heading} subheading={props.subheading}>
                 {props.children}
             </Text>
-            <Image />
+            <Image image={props.image}/>
         </Row>
     </Container>
 );
