ee6565117b497414c92676df984eaa5a6f122d54
Awab Qureshi <awab.qureshi@gmail.com>
2021-07-13 21:02:24 +0500

    Added heading, subheading, children

--- src/components/BodyText.tsx
+++ src/components/BodyText.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 import { Col } from "react-bootstrap";
 
-export const Text: React.FC = props => (
+interface TextProps {
+    children: any,
+    heading: string,
+    subheading?: string,
+}
+
+export const Text: React.FC<TextProps> = props => (
     <Col sm={8}>
+        <h1>{props.heading}</h1>
+        <h4>{props.subheading}</h4>
         {props.children}
     </Col>
 );
